de4b91dc91ed85c73c309393f5c072910816a91a
BVGdragon1025 <72506695+BVGdragon1025@users.noreply.github.com>
2022-05-30 12:08:14 +0200

    Update 1

---
+++ Assets/Scripts/Particle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    //Public Variables
+    public float speed = 3.0f;
+    public float maxZ = 16.0f;
+    public float minZ = -16.0f;
+
+    //Private Variables
+    private int _direction = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, _direction * speed * Time.deltaTime);
+        bool bounced = false;
+        if(transform.position.z > maxZ || transform.transform.position.z < minZ)
+        {
+            bounced = true;
+        }
+        if (bounced){
+            transform.Translate(0, 0, _direction * speed * Time.deltaTime);
+        }
+    }
+}
